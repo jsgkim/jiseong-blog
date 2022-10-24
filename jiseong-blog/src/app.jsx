63a4b36fbfbe2be6f6380cc00ae41b878c268798
Jiseong Kim <jiseong330@gmail.com>
2022-10-24 15:18:30 +0900

    Style fonts

--- jiseong-blog/src/app.jsx
+++ jiseong-blog/src/app.jsx
@@ -1,12 +1,26 @@
 import './app.css';
 import Toggle from './components/toggle';
+import { MdFace } from 'react-icons/md';
 
 function App() {
   return (
-    <header>
-      <h1>Jiseong Blog</h1>
-      <Toggle />
-    </header>
+    <>
+      <header>
+        <h1>Jiseong Blog</h1>
+        <Toggle />
+      </header>
+      <aside>
+        <p>
+          <MdFace /> Personal blog by <a href='https://github.com/jsgkim'>Jiseong Kim</a>.
+        </p>
+      </aside>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </>
   );
 }
 
