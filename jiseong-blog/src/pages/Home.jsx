5cbf7af619300e9e7eeae51064f3bd0efc8d5017
Jiseong Kim <jiseong330@gmail.com>
2022-11-04 11:23:01 +0900

    Style a on hover

--- jiseong-blog/src/pages/Home.jsx
+++ jiseong-blog/src/pages/Home.jsx
@@ -3,8 +3,15 @@ import Toggle from '../components/toggle';
 import { MdFace } from 'react-icons/md';
 import Article from '../components/Article';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Home = () => {
+  const [isHovering, setIsHovering] = useState(false);
+  const handleMouseEnter = () => { setIsHovering(true) };
+  const handleMouseLeave = () => { setIsHovering(false) };
+  const [isHovering2, setIsHovering2] = useState(false);
+  const handleMouseEnter2 = () => { setIsHovering2(true) };
+  const handleMouseLeave2 = () => { setIsHovering2(false) };
   return (
     <div className='wrapper'>
       <header>
@@ -13,16 +20,30 @@ const Home = () => {
       </header>
       <aside>
         <p>
-          <MdFace /> Personal blog by <a href='https://github.com/jsgkim' target="_blank">Jiseong Kim</a>.
+          <MdFace /> Personal blog by&nbsp;
+          <a
+            href='https://github.com/jsgkim'
+            target="_blank"
+            style={{ textDecoration: isHovering ? 'none' : 'underline' }}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>
+            Jiseong Kim
+          </a>.
         </p>
       </aside>
       <main>
         <Link to='/ThinkingInReact'><Article>리액트로 생각하기</Article></Link>
       </main>
       <footer>
-        <a href=''>contact</a>
+        <a
+          href=''
+          style={{ textDecoration: isHovering2 ? 'none' : 'underline' }}
+          onMouseEnter={handleMouseEnter2}
+          onMouseLeave={handleMouseLeave2}>
+          contact
+        </a>
       </footer>
-    </div>
+    </div >
   );
 }
 
